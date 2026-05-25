ea422338deaa1eccae8710c056772db9c18899cd
rjohn2004 <ramonjohn.delacruz@gmail.com>
2026-05-25 09:21:50 +0800

    Updated code for initialization first

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <cstdlib>
 
-
 void printHeader() {
 	std::cout << " ____  ____  ____  ____  _____ ____ ___  _" << std::endl;
 	std::cout << "/   _\\/ ___\\/  _ \\/  __\\/  __// ___\\\\  \\//" << std::endl;
@@ -14,59 +13,54 @@ void printHeader() {
 }
 
 int main() {
-
 	std::string command;
-
-	bool running = true;
+	bool initialized = false;
 
 	printHeader();
 
-	while (running) {
-
+	while (true) {
 		std::cout << "Type Command Here: ";
-		getline(std::cin, command);
+		std::getline(std::cin, command);
+
+		if (command == "exit") {
+			break;
+		}
+
+		if (!initialized && command != "initialize") {
+			std::cout << "Please initialize everything first" << std::endl;
+			std::cout << "---------------------------------------------\n" << std::endl;
+			continue;
+		}
 
 		if (command == "initialize") {
-			std::cout << "Initialize command recognized. Doing something....." << std::endl;
+			std::cout << "Initialize done" << std::endl;
 			std::cout << "---------------------------------------------\n" << std::endl;
+			initialized = true;
 		}
 		else if (command == "screen") {
 			std::cout << "Screen command recognized. Doing something....." << std::endl;
 			std::cout << "---------------------------------------------\n" << std::endl;
 		}
 		else if (command == "scheduler-start") {
-			std::cout << "Scheduler-star command recognized. Doing something....." << std::endl;
+			std::cout << "Scheduler-start command recognized. Doing something....." << std::endl;
 			std::cout << "---------------------------------------------\n" << std::endl;
 		}
-
 		else if (command == "scheduler-stop") {
 			std::cout << "Scheduler-stop command recognized. Doing something....." << std::endl;
 			std::cout << "---------------------------------------------\n" << std::endl;
 		}
-
 		else if (command == "report-util") {
 			std::cout << "Report-util command recognized. Doing something....." << std::endl;
 			std::cout << "---------------------------------------------\n" << std::endl;
 		}
-
 		else if (command == "clear") {
-			system("cls"); //clears console screen but only for Windows
+			system("cls");
 			printHeader();
 		}
-
-		else if (command == "exit") {
-			running = false;
-
-		}
-		else
-		{
+		else {
 			std::cout << "Command is not recognized. Please type again" << std::endl;
 			std::cout << "---------------------------------------------\n" << std::endl;
 		}
 	}
-    return 0;
-
-
-
+	return 0;
 }
-
