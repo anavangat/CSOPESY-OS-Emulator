b848e2ab9ff3bc33c58664022a3df15526fe5e2a
rjohn2004 <ramonjohn.delacruz@gmail.com>
2026-06-27 13:55:59 +0800

    print cmd with sleep

--- SleepInstruction.cpp
+++ SleepInstruction.cpp
@@ -1,10 +1,15 @@
 #include "SleepInstruction.h"
+#include "Process.h"
 
 SleepInstruction::SleepInstruction(int pid, uint8_t ticks) 
 	: Instruction(pid, SLEEP), ticks(ticks) {}
 
 void SleepInstruction::execute(Process& process, SymbolTable& symbolTable) {
 	Instruction::execute(process, symbolTable);
+
+	//Comment this one if not needed or not in specs :>
+	std::string logLine = "Instruction: SLEEP for " + std::to_string(ticks) + " ticks.";
+    process.appendOutput(logLine);
 }
 
 uint8_t SleepInstruction::getTicks() const {
